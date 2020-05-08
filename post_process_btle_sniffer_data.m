55f0b1f2ea6fd937c2e424681c1090e1ef1f2976
klingm <matthew.kling@gmail.com>
2020-05-08 14:49:56 -0400

    Upload data

--- post_process_btle_sniffer_data.m
+++ post_process_btle_sniffer_data.m
@@ -1,4 +1,4 @@
-function [T, fig, s37, s38, s39] = post_process_btle_sniffer_data(filename)
+function [metadata, btle, fig, s37, s38, s39] = post_process_btle_sniffer_data(filename)
 % Post process a data file collected with the btle_sniffer.py script.  Plot
 % data on a channel-by-channel basis.  Return values are:
 %
@@ -8,75 +8,117 @@ function [T, fig, s37, s38, s39] = post_process_btle_sniffer_data(filename)
 %   s38 - object containing stats for ch 38, index as 'min', 'max', etc.
 %   s39 - object containing stats for ch 39, index as 'min', 'max', etc.
 %
-    
-% read csv data into table
-T = readtable(filename, 'FileType', 'text');
+
+fid = fopen(filename,'r');
+
+% read all line from the file
+tline = fgetl(fid);
+btleCnt = 1;
+while ischar(tline)
+    fields = split(tline,',');
+    if strcmp(fields{2},'Environment')
+        % This is an info field containing metadata
+        metadata.timestamp = fields{1};
+        metadata.env = fields{3};
+    elseif strcmp(fields{2},'Device')
+        % This is an info field containing metadata
+        metadata.device = fields{3};
+    elseif strcmp(fields{2},'Range')
+        % This is an info field containing metadata
+        metadata.range = fields{3};
+    elseif strcmp(fields{2},'Angle')
+        % This is an info field containing metadata
+        metadata.angle = fields{3};
+    elseif strcmp(fields{2},'TxPower')
+        % This is an info field containing metadata
+        metadata.txPower = fields{3};
+    elseif strcmp(fields{2},'GPS')
+        % This is an info field containing metadata
+        metadata.gps = fields{3};
+    elseif strcmp(fields{2},'Bluetooth')
+        % This is a data field
+        if length(fields) == 7
+            btle.timestamp{btleCnt} = fields{1};
+            btle.addr{btleCnt} = fields{3};
+            btle.rssi(btleCnt) = str2double(fields{4});
+            btle.txPower(btleCnt) = str2double(fields{5});
+            btle.time(btleCnt) = str2double(fields{6});
+            btle.chan(btleCnt) = str2double(fields{7});
+            btleCnt = btleCnt + 1;
+        else
+            disp('Error, incorrect numer of fields in btle record!')
+        end
+    end
+    tline = fgetl(fid);
+end
+
+fclose(fid);
 
 % filter by BTLE channel
-T_37=T(T.Var2 == 37,:);
-T_38=T(T.Var2 == 38,:);
-T_39=T(T.Var2 == 39,:);
+btle.rssiCh37=btle.rssi(btle.chan == 37);
+btle.rssiCh38=btle.rssi(btle.chan == 38);
+btle.rssiCh39=btle.rssi(btle.chan == 39);
 
 % generate plots
-fig = figure(1); 
+fig = figure; 
 subplot(3,1,1); 
-plot(T_37.Var1, T_37.Var5); 
+plot(btle.rssiCh37); 
 subplot(3,1,2); 
-plot(T_38.Var1, T_38.Var5); 
+plot(btle.rssiCh38); 
 subplot(3,1,3); 
-plot(T_39.Var1, T_39.Var5);
+plot(btle.rssiCh39);
 
 s37 = containers.Map({'min','max','mean','median','mode','std','range'}, ...
-                       [min(T_37.Var5), ...
-                       max(T_37.Var5), ...
-                       mean(T_37.Var5), ...
-                       median(T_37.Var5), ...
-                       mode(T_37.Var5), ...
-                       std(T_37.Var5), ...
-                       range(T_37.Var5)]);
+                       [min(btle.rssiCh37), ...
+                       max(btle.rssiCh37), ...
+                       mean(btle.rssiCh37), ...
+                       median(btle.rssiCh37), ...
+                       mode(btle.rssiCh37), ...
+                       std(btle.rssiCh37), ...
+                       range(btle.rssiCh37)]);
 disp('Ch 37 Stats')
-disp(['min = ' int2str(s37('min'))])
-disp(['max = ' int2str(s37('max'))])
-disp(['mean = ' int2str(s37('mean'))])
-disp(['median = ' int2str(s37('median'))])
-disp(['mode = ' int2str(s37('mode'))])
-disp(['std = ' int2str(s37('std'))])
-disp(['range = ' int2str(s37('range'))])
+disp(['min = ' num2str(s37('min'))])
+disp(['max = ' num2str(s37('max'))])
+disp(['mean = ' num2str(s37('mean'))])
+disp(['median = ' num2str(s37('median'))])
+disp(['mode = ' num2str(s37('mode'))])
+disp(['std = ' num2str(s37('std'))])
+disp(['range = ' num2str(s37('range'))])
 fprintf('\n')
                
 s38 = containers.Map({'min','max','mean','median','mode','std','range'}, ...
-                       [min(T_38.Var5), ...
-                       max(T_38.Var5), ...
-                       mean(T_38.Var5), ...
-                       median(T_38.Var5), ...
-                       mode(T_38.Var5), ...
-                       std(T_38.Var5), ...
-                       range(T_38.Var5)]);
+                       [min(btle.rssiCh38), ...
+                       max(btle.rssiCh38), ...
+                       mean(btle.rssiCh38), ...
+                       median(btle.rssiCh38), ...
+                       mode(btle.rssiCh38), ...
+                       std(btle.rssiCh38), ...
+                       range(btle.rssiCh38)]);
                    
 disp('Ch 38 Stats')
-disp(['min = ' int2str(s38('min'))])
-disp(['max = ' int2str(s38('max'))])
-disp(['mean = ' int2str(s38('mean'))])
-disp(['median = ' int2str(s38('median'))])
-disp(['mode = ' int2str(s38('mode'))])
-disp(['std = ' int2str(s38('std'))])
-disp(['range = ' int2str(s38('range'))])
+disp(['min = ' num2str(s38('min'))])
+disp(['max = ' num2str(s38('max'))])
+disp(['mean = ' num2str(s38('mean'))])
+disp(['median = ' num2str(s38('median'))])
+disp(['mode = ' num2str(s38('mode'))])
+disp(['std = ' num2str(s38('std'))])
+disp(['range = ' num2str(s38('range'))])
 fprintf('\n')
 
 s39 = containers.Map({'min','max','mean','median','mode','std','range'}, ...
-                       [min(T_39.Var5), ...
-                       max(T_39.Var5), ...
-                       mean(T_39.Var5), ...
-                       median(T_39.Var5), ...
-                       mode(T_39.Var5), ...
-                       std(T_39.Var5), ...
-                       range(T_39.Var5)]);
+                       [min(btle.rssiCh39), ...
+                       max(btle.rssiCh39), ...
+                       mean(btle.rssiCh39), ...
+                       median(btle.rssiCh39), ...
+                       mode(btle.rssiCh39), ...
+                       std(btle.rssiCh39), ...
+                       range(btle.rssiCh39)]);
 disp('Ch 39 Stats')
-disp(['min = ' int2str(s39('min'))])
-disp(['max = ' int2str(s39('max'))])
-disp(['mean = ' int2str(s39('mean'))])
-disp(['median = ' int2str(s39('median'))])
-disp(['mode = ' int2str(s39('mode'))])
-disp(['std = ' int2str(s39('std'))])
-disp(['range = ' int2str(s39('range'))])
+disp(['min = ' num2str(s39('min'))])
+disp(['max = ' num2str(s39('max'))])
+disp(['mean = ' num2str(s39('mean'))])
+disp(['median = ' num2str(s39('median'))])
+disp(['mode = ' num2str(s39('mode'))])
+disp(['std = ' num2str(s39('std'))])
+disp(['range = ' num2str(s39('range'))])
 fprintf('\n')
